b2afb44652fa787401ad73df880f5c0a0007bd54
Joejoeflyn <thaitainguyen336@gmail.com>
2025-06-22 23:26:59 +0700

    feat: add kanban board ui

--- zigtask-client/src/pages/tasks.tsx
+++ zigtask-client/src/pages/tasks.tsx
@@ -1,10 +1,13 @@
+import { KanbanBoard } from '@/components/kanban-board';
 import { Navbar } from '@/components/navbar';
 
 export default function Tasks() {
   return (
     <div>
       <Navbar />
-      <div className="p-6">Task page</div>
+      <div className="p-6">
+        <KanbanBoard />
+      </div>
     </div>
   );
 }
